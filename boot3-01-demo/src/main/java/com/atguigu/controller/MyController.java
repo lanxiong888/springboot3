01c01f51957dee9dac22a4562f5dce04f76c2438
lan <773376729@qq.com>
2024-02-29 17:06:59 +0800

    ignore java模板

---
+++ boot3-01-demo/src/main/java/com/atguigu/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.atguigu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hello,Spring Boot 3!";
+    }
+}
